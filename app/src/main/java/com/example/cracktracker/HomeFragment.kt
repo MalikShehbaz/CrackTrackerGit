494de1af03852469c5ee02e4da3b6cd8bde80ff2
Malik Shehbaz <MalikShehbaz8969@gmail.com>
2025-03-21 21:38:54 +0500

    created a drawer and added plus icon

--- app/src/main/java/com/example/cracktracker/HomeFragment.kt
+++ app/src/main/java/com/example/cracktracker/HomeFragment.kt
@@ -1,59 +1,47 @@
 package com.example.cracktracker
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.provider.MediaStore
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class FragmentHome : Fragment(R.layout.fragment_home) {
+    private var drawerLayout: DrawerLayout? = null
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        val menuButton = view.findViewById<ImageView>(R.id.menuButton)
+        val addButton = view.findViewById<ImageView>(R.id.btn_add) // Plus button
+        drawerLayout = activity?.findViewById(R.id.drawer_layout)
+
+        // Open drawer when menu button is clicked
+        menuButton?.setOnClickListener {
+            drawerLayout?.openDrawer(GravityCompat.START)
+        }
+
+        // Open camera only if a camera app is available
+        addButton?.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val packageManager = activity?.packageManager
+
+            if (cameraIntent.resolveActivity(packageManager!!) != null) {
+                startActivity(cameraIntent)
+            } else {
+                Toast.makeText(requireContext(), "No camera app found!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onResume() {
+        super.onResume()
+        // Completely disable drawer swipe gesture
+        drawerLayout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
     }
 }
--- app/src/main/java/com/example/cracktracker/MainActivity.kt
+++ app/src/main/java/com/example/cracktracker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.cracktracker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize DrawerLayout
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        // Disable swipe gesture for opening the drawer
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+    }
+
+    // Function to open the drawer from fragments
+    fun openDrawer() {
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED) // Temporarily unlock
+        drawerLayout.openDrawer(GravityCompat.START)
     }
 }
